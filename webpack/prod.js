fa60b371b447daeef7ede3e69b6a3b0d28d34785
Bernardo Bertante <bernardo.sctp@gmail.com>
2024-12-16 11:43:23 -0300

    fix: error trying to launch on web

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,67 +1,70 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    devtool: false,
-    output: {
-        filename: 'bundle.min.js'
-    },
-    performance: {
-        maxEntrypointSize: 900000,
-        maxAssetSize: 900000
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    output: {
-                        comments: false
-                    }
-                }
-            })
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: [/\.vert$/, /\.frag$/],
-                use: 'raw-loader'
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg|xml)$/i,
-                use: 'file-loader'
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            root: path.resolve(__dirname, '../')
-        }),
-        new CopyPlugin([
-            { from: 'src/assets', to: 'assets' },
-        ]),
-        new webpack.DefinePlugin({
-            "typeof CANVAS_RENDERER": JSON.stringify(true),
-            "typeof WEBGL_RENDERER": JSON.stringify(true),
-            "typeof EXPERIMENTAL": JSON.stringify(false),
-            "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
-            "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
-            "typeof FEATURE_SOUND": JSON.stringify(true)
-        }),
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        })
-    ]
+  mode: "production",
+  devtool: false,
+  entry: "./src/index.js", // Entrada principal do seu projeto
+  output: {
+    path: path.resolve(__dirname, "../build"), // Pasta de saída
+    filename: "bundle.min.js", // Nome do arquivo gerado
+  },
+  performance: {
+    maxEntrypointSize: 900000,
+    maxAssetSize: 900000,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false, // Remove comentários
+          },
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader", // Transpila o JS
+        },
+      },
+      {
+        test: [/\.vert$/, /\.frag$/], // Para shaders
+        use: "raw-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i, // Para imagens e XML
+        use: "file-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" }, // Copia os arquivos de assets
+      ],
+    }),
+    new webpack.DefinePlugin({
+      "typeof CANVAS_RENDERER": JSON.stringify(true),
+      "typeof WEBGL_RENDERER": JSON.stringify(true),
+      "typeof EXPERIMENTAL": JSON.stringify(false),
+      "typeof PLUGIN_CAMERA3D": JSON.stringify(false),
+      "typeof PLUGIN_FBINSTANT": JSON.stringify(false),
+      "typeof FEATURE_SOUND": JSON.stringify(true),
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html", // Certifique-se de que o HTML está na pasta src
+      filename: "index.html", // Nome do arquivo gerado
+    }),
+  ],
 };
